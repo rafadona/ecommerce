fc59a6d3f1093df87210c6966052a80bebeac641
Rafael <rafa_dona@hotmail.com>
2021-09-20 19:56:28 -0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Rafael Ecommerce</title>
+      </Head>
+
+      <h1>TEste</h1>
+    </div>
+  );
+}
