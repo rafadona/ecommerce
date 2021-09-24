f6b81a3022346619ebdb56c6b585c79a94cf8953
Rafael <rafa_dona@hotmail.com>
2021-09-24 16:46:58 -0300

    footer melhorado

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,38 +12,38 @@ function Footer() {
 
 
     return (
-        <footer className="bottom-0">
+        <footer className="bottom-0 text-xs md:text-sm">
             <div className="flex flex-col">
-                <button title="Para o topo" onClick={scrollToTop} className="bg-gray-400 hover:bg-gray-700 p-3">
+                <button title="Para o topo" onClick={scrollToTop} className="bg-gray-500 hover:bg-gray-700 p-3 font-semibold">
                     Voltar ao início
 
                 </button>
             </div>
 
-            <div className="text-center bg-gray-500 flex flex-col p-3">
-                <h1>teste footer</h1>
+            <div className="text-center bg-gray-600 flex flex-col p-3 space-y-2">
+                <h1 className="font-bold">Teste footer</h1>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum iure perspiciatis quia reprehenderit odit unde quaerat itaque eligendi sit quibusdam possimus animi ab, aperiam at cupiditate? Quisquam praesentium est accusamus.</p>
+                <p>
+                    Formas de pagamento aceitas: cartões de crédito (Visa, MasterCard, Elo e American Express) e boleto.
+                </p>
             </div>
 
             <div className="bg-gray-700 p-4">
-                <h1 className="text-center">Criado por <a className="link" href="mailto:rafa_dona@hotmail.com">Rafael Doná</a> </h1>
-                <ul className="flex space-x-6 my-3 justify-center ">
+                <h1 className="text-center font-semibold">Criado por <a className="link text-gray-400 hover:text-gray-100" href="mailto:rafa_dona@hotmail.com">Rafael Doná</a> </h1>
+                <ul className="flex space-x-6 my-3 justify-center text-gray-400 ">
                     <li>
                         <a href="https://github.com/rafadona" target="_blank">
 
-                            <FontAwesomeIcon className="h-10" icon={faGithub} size="4x" />
+                            <FontAwesomeIcon className="h-10 hover:text-gray-100" icon={faGithub} size="4x" />
                         </a>
                     </li>
                     <li>
                         <a href="https://www.linkedin.com/in/rafael-dona/" target="_blank">
 
-                            <FontAwesomeIcon className="h-10" icon={faLinkedin} size="4x" />
+                            <FontAwesomeIcon className="h-10 hover:text-gray-100" icon={faLinkedin} size="4x" />
                         </a>
                     </li>
                 </ul>
-
-
-
             </div>
         </footer>
     );
