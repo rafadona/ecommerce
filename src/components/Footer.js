d7465f501dc5373d1437d15b73122536d13ae01b
Rafael <rafa_dona@hotmail.com>
2021-09-28 18:30:38 -0300

    pagina detalhes e produtos relacionados funcionand

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
-import { ArrowCircleUpIcon } from "@heroicons/react/solid";
 
 function Footer() {
 
--- src/pages/produtos/[id].js
+++ src/pages/produtos/[id].js
@@ -2,10 +2,11 @@ import Image from "next/dist/client/image";
 import { useDispatch } from "react-redux";
 import Header from "../../components/Header";
 import { addToBasket } from "../../slices/basketSlice";
+import Footer from "../../components/Footer";
+import Produto from "../../components/Produto";
 
 
-
-function PaginaDetalhes({ produto }) {
+function PaginaDetalhes({ produto, produtosCategoria, categoria }) {
     const dispatch = useDispatch();
 
     const addItemToBasket = () => {
@@ -24,6 +25,13 @@ function PaginaDetalhes({ produto }) {
     };
 
 
+    const produtosRelacionados = produtosCategoria.filter((item) => item.id != produto.id && item.category === produto.category).slice(0, 4);
+
+
+
+    const testeProdutos = () => {
+        console.log(produtosCategoria);
+    };
 
 
     return (
@@ -33,38 +41,67 @@ function PaginaDetalhes({ produto }) {
 
 
 
-            <div className="mt-6 max-w-2xl mx-auto sm:px-2 lg:max-w-6xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-x-2 border border-gray-600">
-                <div className="aspect-w-4 aspect-h-5 sm:rounded-lg sm:overflow-hidden lg:aspect-w-3 lg:aspect-h-4">
-                    <img
+            <div className="mt-6 max-w-2xl mx-auto sm:px-2 lg:max-w-6xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-x-2">
+                <div className="aspect-w-4 aspect-h-5  sm:rounded-lg sm:overflow-hidden lg:aspect-w-3 lg:aspect-h-4">
+                    <Image
                         src={produto.image}
-                        alt={produto.image}
+                        alt={produto.title}
+                        width={400} height={400}
+                        objectFit='contain'
                         className="w-full h-full object-center object-cover"
                     />
                 </div>
 
 
-                <div className="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-6xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
+                <div className="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-6xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-2 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
                     <div className="lg:col-span-2  lg:pr-8">
                         <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">{produto.title}</h1>
                     </div>
-                    <div className="py-10 lg:pt-6 lg:pb-16 lg:col-start-1 lg:col-span-2 lg:pr-8">
-                        {/* Description and details */}
+                    <div className="py-6 lg:pt-6 lg:pb-10 lg:col-start-1 lg:col-span-2 lg:pr-8">
+                        {/* Descricao e detalhes */}
                         <div>
-                            <h3 className="font-bold">Descrição</h3>
+                            <h3 className="font-bold mb-1">Descrição</h3>
 
                             <div className="space-y-6">
                                 <p className="text-base text-gray-900">{produto.description}</p>
+                                <p className="font-bold">R$ {produto.price}</p>
                             </div>
                         </div>
                     </div>
+
+                    <button onClick={addItemToBasket} className="w-full text-xl button lg:col-span-2 lg:pr-8 ">Adicionar ao Carrinho</button>
+
+
+                </div>
+
+
+            </div>
+
+
+            <div className=" border-black bg-gray-200">
+                <h2 className="font-bold mt-2 pt-2">Outros clientes também visualizaram esses produtos</h2>
+                <div className="relative grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto">
+                    {produtosRelacionados.map(({ id, title, price, description, category, image }) => (
+                        <Produto
+                            key={id}
+                            id={id}
+                            title={title}
+                            price={price}
+                            category={category}
+                            image={image}
+                            description={description}
+                        />
+                    ))}
 
                 </div>
 
             </div>
 
-            <button onClick={addItemToBasket} className="mt-auto button">Adicionar ao Carrinho</button>
 
 
+
+
+            <Footer />
 
         </div>
 
@@ -84,6 +121,7 @@ export async function getStaticPaths() {
         paths: produtos.map((produto) => ({
             params: {
                 id: produto.id.toString(),
+
             }
         })),
         fallback: false,
@@ -96,12 +134,18 @@ export async function getStaticProps(context) {
     const id = context.params.id;
 
 
+
     const res = await fetch(`https://fakestoreapi.com/products/${id}`);
     const produto = await res.json();
+    const resCategoria = await fetch(`https://fakestoreapi.com/products/`);
+    const produtosCategoria = await resCategoria.json();
 
     return {
         props: {
             produto,
+            produtosCategoria,
+
+
         }
     };
 
